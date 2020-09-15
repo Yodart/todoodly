ea86358d5d7383acd35fc93a18a941e518969b93
Yodart <joaogabrielgcoutinho@gmail.com>
2020-09-14 21:29:14 -0300

    Login & Signups

--- signup/script.js
+++ signup/script.js
@@ -2,15 +2,20 @@ const signup = async () => {
   const response = await fetch("http://127.0.0.1:5000/user", {
     method: "POST",
     body: JSON.stringify({
-      name: document.getElementById("nameInput").value,
-      password: document.getElementById("passwordInput").value,
+      name: document.getElementById("name-input").value,
+      password: document.getElementById("password-input").value,
     }),
   });
   if (response.status == 200) {
     const json = await response.json();
     document.cookie = json["token"];
-    document.getElementById("errorMessage").innerHTML = "Account Created";
+    document.getElementById("success-alert").style.display = "block";
   } else {
-    document.getElementById("errorMessage").innerHTML = "Wrong Password";
+    document.getElementById("failed-alert").style.display = "block";
   }
+};
+
+const onInputChange = () => {
+  document.getElementById("success-alert").style.display = "none";
+  document.getElementById("failed-alert").style.display = "none";
 };
